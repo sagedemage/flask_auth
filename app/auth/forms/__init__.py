a74f5353690017a3267dba0772aa97fc4d7e01e7
sagedemage <salsaeed999@gmail.com>
2022-03-22 17:41:59 -0400

    first commit

---
+++ app/auth/forms/__init__.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import validators
+from wtforms.fields import *
+
+
+class login_form(FlaskForm):
+    """ Login Form """
+    email = EmailField('Email Address', [
+        validators.DataRequired(),
+    ])
+
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+    ])
+    submit = SubmitField()
+
+
+class register_form(FlaskForm):
+    """ Registration Template """
+    email = EmailField('Email Address', [
+        validators.DataRequired(),
+
+    ],description="You need to signup with an email")
+
+    # Enter password
+    password = PasswordField('Create Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.length(min=6, max=35)
+    ],description="Create a password ")
+
+    # Reenter password
+    confirm = PasswordField('Repeat Password',description="Please retype your password to confirm it is correct")
+    submit = SubmitField()
