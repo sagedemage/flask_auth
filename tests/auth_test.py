076006f7a15b4fda1856e8c011491b9a07de74bd
sagedemage <salsaeed999@gmail.com>
2022-03-23 17:00:40 -0400

    Added comments to the auth test program to set up a general plan for this program.

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,4 +1,8 @@
 """This test the homepage"""
+from flask import g
+from flask import session
+from app.auth import login, register, dashboard, logout
+
 
 def test_request_main_menu_links(client):
     """This makes the index page"""
@@ -6,6 +10,7 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
 
 def test_auth_pages(client):
     """This makes the index page"""
@@ -15,3 +20,47 @@ def test_auth_pages(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+
+def test_login(client):
+    """ Test Bad login """
+    response = client.get("/login")
+    
+    """ There should be a way to pass in values for
+     the username and password to test login """
+
+    # Bad username
+    assert 1 == 1
+
+    # Bad password
+    assert 2 == 2
+
+    # Login success
+    assert 3 == 3
+
+
+def test_registration(client):
+    """ Test Bad Registration """
+    response = client.get("/register")
+
+    """ There should be a way to pass in values for
+    the username and password to test registration """
+
+    # Bad username
+    assert 1 == 1
+    # Password Confirmation
+    assert 2 == 2
+    # Bad password
+    assert 3 == 3
+    # Already registered
+    assert 4 == 4
+
+    # Registration success
+    assert 5 == 5
+
+
+def user_access_to_dashboard(client):
+    # Deny access to the dashboard for login users
+    assert 1 == 1
+    # Allow access to the dashboard for login users
+    assert 2 == 2
