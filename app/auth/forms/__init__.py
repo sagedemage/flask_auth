4874a819d2eabb0a01b982418bcf293dec3eab6d
sagedemage <salsaeed999@gmail.com>
2022-03-23 12:01:53 -0400

    testing

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import validators
 from wtforms.fields import *
 
-
+# test
 class login_form(FlaskForm):
     """ Login Form """
     email = EmailField('Email Address', [
