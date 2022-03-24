e037150a2f751690caca8afdb2824c8bafee8d37
sagedemage <salsaeed999@gmail.com>
2022-03-23 23:38:39 -0400

    added tests for login success and registration success

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,7 +1,6 @@
 """This test the homepage"""
-from flask import g
-from flask import session
-from app.auth import login, register, dashboard, logout
+import pytest
+from flask import g, session
 
 
 def test_request_main_menu_links(client):
@@ -14,53 +13,19 @@ def test_request_main_menu_links(client):
 
 def test_auth_pages(client):
     """This makes the index page"""
-    response = client.get("/dashboard")
-    assert response.status_code == 302
-    response = client.get("/register")
-    assert response.status_code == 200
-    response = client.get("/login")
+    assert client.get("/dashboard").status_code == 302
+    assert client.get("/register").status_code == 200
+    assert client.get("/login").status_code == 200
+
+
+def test_registration_success(client):
+    """ Registration """
+    response = client.post("/register", data={"email": "sal@gmail.com", "password": "123456"})
     assert response.status_code == 200
 
 
-def test_login(client):
+def test_login_success(client):
     """ Test Bad login """
-    response = client.get("/login")
-    
-    """ There should be a way to pass in values for
-     the username and password to test login """
-
-    # Bad username
-    assert 1 == 1
-
-    # Bad password
-    assert 2 == 2
-
-    # Login success
-    assert 3 == 3
-
-
-def test_registration(client):
-    """ Test Bad Registration """
-    response = client.get("/register")
-
-    """ There should be a way to pass in values for
-    the username and password to test registration """
-
-    # Bad username
-    assert 1 == 1
-    # Password Confirmation
-    assert 2 == 2
-    # Bad password
-    assert 3 == 3
-    # Already registered
-    assert 4 == 4
-
-    # Registration success
-    assert 5 == 5
-
-
-def user_access_to_dashboard(client):
-    # Deny access to the dashboard for login users
-    assert 1 == 1
-    # Allow access to the dashboard for login users
-    assert 2 == 2
+    # Login
+    response = client.post("/login", data={"email": "sal@gmail.com", "password": "123456"})
+    assert response.status_code == 200
