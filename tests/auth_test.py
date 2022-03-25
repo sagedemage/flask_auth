998af89d3811ee60f394cd4599ebe9c9f69e7d8e
sagedemage <salsaeed999@gmail.com>
2022-03-25 00:57:46 -0400

    logout test works but registration and login tests do not work

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -18,21 +18,39 @@ def test_auth_pages(client):
     assert client.get("/login").status_code == 200
 
 
-def test_registration_success(client):
+@pytest.mark.parametrize(
+    ("email", "password"),
+    (("test1234@gmail.com", "test1234"),),
+)
+def test_registration_success(client, email, password):
     """ Registration """
-    response = client.get("/register", data={"email": "test1234@gmail.com", "password": "test1234"})
-    # The html status code should be a 300 (redirect) but it is not
+    # This test does not work properly
+    # The page keeps getting a 400 BAD REQUEST status code
+    response = client.post("/register", data={"email": email, "password": password})
     assert response.status_code == 200
 
 
-def test_login_success(client):
+@pytest.mark.parametrize(
+    ("email", "password"),
+    (("test1234@gmail.com", "test1234"),),
+)
+def test_login_success(client, email, password):
     """ Login """
-    response = client.get("/login", data={"email": "test1234@gmail.com", "password": "test1234"})
-    # The html status code should be a 300 (redirect) but it is not
+    # This test does not work properly
+    # The page keeps getting a 400 BAD REQUEST status code
+    response = client.post("/login", data={"email": email, "password": password})
     assert response.status_code == 200
 
 
 def test_logout(client):
     """ Logout """
-    response = client.get("/logout")
-    assert response.status_code == 302
+    response = client.get("/logout", follow_redirects=True)
+    assert len(response.history) == 1
+    assert response.request.path == "/login"
+
+
+def test_deny_access_to_dashboard(client):
+    """ Deny access to the Dashboard """
+    # Ensure that the client does not have access
+    # to the dashboard without logging in
+    assert client.get("/dashboard").status_code == 302
