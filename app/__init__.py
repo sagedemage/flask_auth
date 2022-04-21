c7ad45eef1c2696d27f25e27448607587dfc7d59
sagedemage <salsaeed999@gmail.com>
2022-04-21 00:25:15 -0400

    added the location functionality: the user can upload a csv file that contains locations and the web app can display a table of locations

--- app/__init__.py
+++ app/__init__.py
@@ -1,64 +1,63 @@
 """A simple flask web app"""
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 import flask_login
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
 
-import os
-from flask import Flask
+from app.auth import auth
+from app.cli import create_database, create_log_folder
 from app.context_processors import utility_text_processors
-from app.simple_pages import simple_pages
-from app.auth import auth
-from app.exceptions import http_exceptions
-from app.db.models import User
 from app.db import db
-from app.auth import auth
-from app.cli import create_database
-from flask_login import (
-    UserMixin,
-    login_user,
-    LoginManager,
-    current_user,
-    logout_user,
-    login_required,
-)
-import logging
+from app.db.models import User
+from app.error_handlers import error_handlers
+from app.logging_config import log_con
+from app.simple_pages import simple_pages
+from app.map import map
 
-login_manager = LoginManager()
-
-
-def page_not_found(e):
-    return render_template("404.html"), 404
+login_manager = flask_login.LoginManager()
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    logging.basicConfig(filename='logs/record.log', level=logging.DEBUG,
-                        format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+    if app.config["ENV"] == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif app.config["ENV"] == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif app.config["ENV"] == "testing":
+        app.config.from_object("app.config.TestingConfig")
 
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    # login manager
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
+
+    # CSRF protection of form submissions
     csrf = CSRFProtect(app)
     csrf.exempt(auth)
+
+    # Flask Bootstrap
     bootstrap = Bootstrap5(app)
+
+    # load functions with web interfaces
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
-    app.context_processor(utility_text_processors)
-    app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
-    app.config['WTF_CSRF_ENABLED'] = False
-    app.register_error_handler(404, page_not_found)
-    # app.add_url_rule("/", endpoint="index")
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    db.init_app(app)
+    # load functionality without a web interface
+    app.register_blueprint(log_con)
+    app.register_blueprint(error_handlers)
+    app.register_blueprint(map)
+    app.context_processor(utility_text_processors)
+
     # add command function to cli commands
     app.cli.add_command(create_database)
-    # Setup Flask-User and specify the User data-model
+    app.cli.add_command(create_log_folder)
+    db.init_app(app)
 
+    # Run once at startup
     return app
 
 
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,35 +1,14 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
-from sqlalchemy.orm import load_only
 from werkzeug.security import generate_password_hash
+
+from app.auth.decorators import admin_required
 from app.auth.forms import login_form, register_form, profile_form, security_form, user_edit_form
 from app.db import db
 from app.db.models import User
-from app.auth.decorators import admin_required
+
 auth = Blueprint('auth', __name__, template_folder='templates')
 from flask import current_app
-
-
-
-
-@auth.route('/login', methods=['POST', 'GET'])
-def login():
-    form = login_form()
-    if current_user.is_authenticated:
-        return redirect(url_for('auth.dashboard'))
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid email or password')
-            return redirect(url_for('auth.login'))
-        else:
-            user.authenticated = True
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            flash("Welcome to the dashboard", 'success')
-            return redirect(url_for('auth.dashboard'))
-    return render_template('login.html', form=form)
 
 
 @auth.route('/register', methods=['POST', 'GET'])
@@ -55,10 +34,24 @@ def register():
     return render_template('register.html', form=form)
 
 
-@auth.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    form = login_form()
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.dashboard'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('auth.login'))
+        else:
+            user.authenticated = True
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            flash("Welcome to the dashboard", 'success')
+            return redirect(url_for('auth.dashboard'))
+    return render_template('login.html', form=form)
 
 
 @auth.route("/logout")
@@ -71,6 +64,39 @@ def logout():
     db.session.commit()
     logout_user()
     return redirect(url_for('auth.login'))
+
+
+@auth.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@auth.route('/profile', methods=['POST', 'GET'])
+def edit_profile():
+    user = User.query.get(current_user.get_id())
+    form = profile_form(obj=user)
+    if form.validate_on_submit():
+        user.about = form.about.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('You Successfully Updated your Profile', 'success')
+        return redirect(url_for('auth.dashboard'))
+    return render_template('profile_edit.html', form=form)
+
+
+@auth.route('/account', methods=['POST', 'GET'])
+def edit_account():
+    user = User.query.get(current_user.get_id())
+    form = security_form(obj=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('You Successfully Updated your Password or Email', 'success')
+        return redirect(url_for('auth.dashboard'))
+    return render_template('manage_account.html', form=form)
 
 
 @auth.route('/users')
@@ -141,31 +167,5 @@ def delete_user(user_id):
     flash('User Deleted', 'success')
     return redirect(url_for('auth.browse_users'), 302)
 
-
-@auth.route('/profile', methods=['POST', 'GET'])
-def edit_profile():
-    user = User.query.get(current_user.get_id())
-    form = profile_form(obj=user)
-    if form.validate_on_submit():
-        user.about = form.about.data
-        db.session.add(current_user)
-        db.session.commit()
-        flash('You Successfully Updated your Profile', 'success')
-        return redirect(url_for('auth.dashboard'))
-    return render_template('profile_edit.html', form=form)
-
-
-@auth.route('/account', methods=['POST', 'GET'])
-def edit_account():
-    user = User.query.get(current_user.get_id())
-    form = security_form(obj=user)
-    if form.validate_on_submit():
-        user.email = form.email.data
-        user.password = form.password.data
-        db.session.add(current_user)
-        db.session.commit()
-        flash('You Successfully Updated your Password or Email', 'success')
-        return redirect(url_for('auth.dashboard'))
-    return render_template('manage_account.html', form=form)
 
 
